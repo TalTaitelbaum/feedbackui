0b13ef55b9bb7a668a5da0ba202827f4da281857
Tal Taitelbaum <talbak.tb@gmail.com>
2023-02-04 18:17:45 +0200

    installing npm prptypes, adding CSS code to index.css file and adding proptypes Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,30 @@
-function Header() {
+import PropTypes from "prop-types";
+
+function Header({ text, bgColor, textColor }) {
+  const headerStyle = {
+    backgroundColor: bgColor,
+    color: textColor,
+  };
+
   return (
-    <Header>
+    <header style={headerStyle}>
       <div className="container">
-        <h2>Feedback UL</h2>
+        <h2>{text}</h2>
       </div>
-    </Header>
+    </header>
   );
 }
+
+Header.defaultProps = {
+  text: "Feedback UI",
+  bgColor: "rgba(0,0,0,0.4)",
+  textColor: "#ff6a95",
+};
+
+Header.propTypes = {
+  text: PropTypes.string,
+  bgColor: PropTypes.string,
+  textColor: PropTypes.string,
+};
 
 export default Header;
