8c93a70fc13f3fa752c18ef7c16a2ce099304847
Tal Taitelbaum <talbak.tb@gmail.com>
2023-02-28 18:12:57 +0200

    adding AboutIconLink component

---
+++ src/components/AboutIconLink.jsx
@@ -0,0 +1,11 @@
+import { FaQuestion } from "react-icon/fa";
+
+function AboutIconLink() {
+  return (
+    <div className="about-link">
+      <FaQuestion size={30} />
+    </div>
+  );
+}
+
+export default AboutIconLink;
