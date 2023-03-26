33ad70d3ba0fe39ed341a74aa3f5fc36f9929004
Tal Taitelbaum <talbak.tb@gmail.com>
2023-03-26 16:05:12 +0300

    Get Global State With The useContext Hook

--- src/components/FeedbackStats.jsx
+++ src/components/FeedbackStats.jsx
@@ -1,6 +1,9 @@
-import PropTypes from "prop-types";
+import { useContext } from "react";
+import FeedbackContext from "../context/FeedbackContext";
 
-function FeedbackStats({ feedback }) {
+function FeedbackStats() {
+  const { feedback } = useContext(FeedbackContext);
+
   //calculate ratings avg
   let average =
     feedback.reduce((acc, cur) => {
@@ -16,9 +19,5 @@ function FeedbackStats({ feedback }) {
     </div>
   );
 }
-
-FeedbackStats.protoType = {
-  feedback: PropTypes.array.isRequired,
-};
 
 export default FeedbackStats;
