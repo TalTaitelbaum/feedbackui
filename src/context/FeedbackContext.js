6fb3f43afcb04014cf53c4bfaf361514848d508b
Tal Taitelbaum <talbak.tb@gmail.com>
2023-04-23 12:09:04 +0300

    edit feedback event

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -22,15 +22,27 @@ export const FeedbackProvider = ({ children }) => {
     },
   ]);
 
+  const [FeedbackEdit, setFeedbackEdit] = useState({ item: {}, edit: false });
+
+  // Add feedback
   const addFeedback = (newFeedback) => {
     newFeedback.id = uuidv4();
     setFeedback([newFeedback, ...feedback]);
   };
 
+  // Delete feedback
   const deleteFeedback = (id) => {
     if (window.confirm("Are you sure you want to delete")) {
       setFeedback(feedback.filter((item) => item.id !== id));
     }
+  };
+
+  //set item to be updated
+  const editFeedback = (item) => {
+    setFeedbackEdit({
+      item,
+      edit: true,
+    });
   };
 
   return (
@@ -39,6 +51,7 @@ export const FeedbackProvider = ({ children }) => {
         feedback,
         deleteFeedback,
         addFeedback,
+        editFeedback,
       }}
     >
       {children}
