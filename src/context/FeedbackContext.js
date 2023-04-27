6813b372c78123a82cbff3b188536666d8cbdbb3
Tal Taitelbaum <talbak.tb@gmail.com>
2023-04-27 16:05:28 +0300

    Update Feedback item

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -37,6 +37,13 @@ export const FeedbackProvider = ({ children }) => {
     }
   };
 
+  //Update feedback item
+  const updateFeedback = (id, updItem) => {
+    setFeedback(
+      feedback.map((item) => (item.id === id ? { ...item, ...updItem } : item))
+    );
+  };
+
   //set item to be updated
   const editFeedback = (item) => {
     setFeedbackEdit({
@@ -49,10 +56,11 @@ export const FeedbackProvider = ({ children }) => {
     <FeedbackContext.Provider
       value={{
         feedback,
+        feedbackEdit,
         deleteFeedback,
         addFeedback,
         editFeedback,
-        feedbackEdit,
+        updateFeedback,
       }}
     >
       {children}
